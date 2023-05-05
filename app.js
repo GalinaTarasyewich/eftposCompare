1be9c22aafeb8f651d17e3beb6da5f85f2c62248
GalinaTarasyewich <hello@balletgalina.com>
2023-05-05 11:07:59 +1000

    changes to all the docs

--- app.js
+++ app.js
@@ -54,3 +54,164 @@ $('.postalAddress').change(function() {
     $('.postalAddressHide').hide();
   }
 });
+
+
+$('.pricingHighRisk').change(function() {
+  if ($(this).val() === 'Auto Parts Stores' || $(this).val() === 'Mechanics' || $(this).val() === 'Tyre Stores') {
+    $('.commentsHide').show();
+  } else {
+    $('.commentsHide').hide();
+  }
+});
+
+
+$('.director1PostalAddress').change(function() {
+  if ($(this).val() === 'Yes') {
+    $('.director1PostalHide').show();
+  } else {
+    $('.director1PostalHide').hide();
+  }
+});
+
+$('.director2PostalAddress').change(function() {
+  if ($(this).val() === 'Yes') {
+    $('.director2PostalHide').show();
+  } else {
+    $('.director2PostalHide').hide();
+  }
+});
+
+$('.director3PostalAddress').change(function() {
+  if ($(this).val() === 'Yes') {
+    $('.director3PostalHide').show();
+  } else {
+    $('.director3PostalHide').hide();
+  }
+});
+
+$('.director4PostalAddress').change(function() {
+  if ($(this).val() === 'Yes') {
+    $('.director4PostalHide').show();
+  } else {
+    $('.director4PostalHide').hide();
+  }
+});
+
+$('.howManyDirectors').change(function() {
+  if ($(this).val() === '4') {
+    $('.director4Hide').show();
+  } else {
+    $('.director4Hide').hide();
+  }
+});
+
+$('.howManyDirectors').change(function() {
+  if ($(this).val() === '3' || $(this).val() === '4') {
+    $('.director3Hide').show();
+  } else {
+    $('.director3Hide').hide();
+  }
+});
+
+$('.howManyDirectors').change(function() {
+  if ($(this).val() === '2' || $(this).val() === '3' || $(this).val() === '4') {
+    $('.director2Hide').show();
+  } else {
+    $('.director2Hide').hide();
+  }
+});
+
+
+
+
+// changes fees according to pricing selection
+function showPricing() {
+  var select = document.getElementById("pricing");
+  var merchantServiceFee = document.getElementById("merchantServiceFee");
+  var debitCardFee = document.getElementById("debitCardFee");
+  var comments1 = document.getElementById("comments1");
+
+  var selectedOption = select.options[select.selectedIndex].value;
+  switch (selectedOption) {
+    case "Flat Rate Offer":
+      merchantServiceFee.value = "0.88%";
+      debitCardFee.value = "0.88%";
+      break;
+    case "Pricing approved by Product":
+      merchantServiceFee.value = "0.82%";
+      debitCardFee.value = "0.82%";
+      comments1.value = "RATES ARE INDUSTRY APPROVED"
+      break;
+    case "Auto Parts Stores":
+      merchantServiceFee.value = "0.49%";
+      debitCardFee.value = "0.49%";
+      comments1.value = "RATES ARE INDUSTRY APPROVED"
+
+      break;
+
+    case "Bakeries":
+      merchantServiceFee.value = "0.78%";
+      debitCardFee.value = "0.78%";
+      comments1.value = "RATES ARE INDUSTRY APPROVED"
+
+      break;
+    case "Butchers":
+      merchantServiceFee.value = "0.49%";
+      debitCardFee.value = "0.49%";
+      comments1.value = "RATES ARE INDUSTRY APPROVED"
+
+      break;
+    case "Hairdressers and Beauticians":
+      merchantServiceFee.value = "0.59%";
+      debitCardFee.value = "0.59%";
+      comments1.value = "RATES ARE INDUSTRY APPROVED"
+
+      break;
+    case "Liquor Stores":
+      merchantServiceFee.value = "0.49%";
+      debitCardFee.value = "0.49%";
+      comments1.value = "RATES ARE INDUSTRY APPROVED"
+
+      break;
+    case "Mechanics":
+      merchantServiceFee.value = "0.47%";
+      debitCardFee.value = "0.47%";
+      comments1.value = "RATES ARE INDUSTRY APPROVED"
+
+      break;
+    case "Petrol Stations":
+      merchantServiceFee.value = "0.35%";
+      debitCardFee.value = "0.35%";
+      comments1.value = "RATES ARE INDUSTRY APPROVED"
+
+      break;
+    case "Pharmacy":
+      merchantServiceFee.value = "0.55%";
+      debitCardFee.value = "0.55%";
+      comments1.value = "RATES ARE INDUSTRY APPROVED"
+
+      break;
+    case "Supermarkets (Buying group members)":
+      merchantServiceFee.value = "0.39%";
+      debitCardFee.value = "0.39%";
+      comments1.value = "RATES ARE INDUSTRY APPROVED"
+
+      break;
+    case "Tobacconist":
+      merchantServiceFee.value = "0.33%";
+      debitCardFee.value = "0.33%";
+      comments1.value = "RATES ARE INDUSTRY APPROVED"
+
+      break;
+    case "Tyre Stores":
+      merchantServiceFee.value = "0.35%";
+      debitCardFee.value = "0.35%";
+      comments1.value = "RATES ARE INDUSTRY APPROVED"
+
+      break;
+    default:
+      merchantServiceFee.value = "";
+      debitCardFee.value = "";
+
+  }
+}
