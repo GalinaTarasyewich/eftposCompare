383c499a46f872fdad14144a274f29c9f44f6972
GalinaTarasyewich <hello@balletgalina.com>
2023-05-25 14:49:31 +1000

    add high risk comments

--- noflat.js
+++ noflat.js
@@ -525,3 +525,115 @@ $dob4.bind('keyup','keydown', function(e){
 		}
   }
 });
+
+
+
+function showHighRiskComments() {
+  var mccInput = document.getElementsByName("mcc")[0];
+  var highRiskCommentsInput = document.getElementsByName("highRiskComments")[0];
+  var selectedOption = mccInput.value;
+
+  if (selectedOption === "0780 - LOW - LANDSCAPE/HORTICULTURAL SERVICES" ||
+    selectedOption === "1520 - LOW - GENERAL CONTRACTORS RESIDENTIAL/COMMERCIAL" ||
+    selectedOption === "1711 - LOW - HEATING/PLUMBING AND AIR CONDITIONING CONTRACTORS" ||
+    selectedOption === "1731 - LOW - ELECTRICAL CONTRACTORS" ||
+    selectedOption === "1740 - LOW - MASONRY/TILE/PLASTER/INSULATION CONTRACTORS" ||
+    selectedOption === "1750 - LOW - CARPENTRY CONTRACTORS" ||
+    selectedOption === "1761 - LOW - ROOFING/SIDING/SHEET METAL CONTRACTORS" ||
+    selectedOption === "1771 - HIGH - CONTRACTORS - CONCRETE" ||
+    selectedOption === "1799 - LOW - CONTRACTORS-SPECIAL TRADE - (NOT CLASSIFIED ELSEWHERE)" ||
+    selectedOption === "3501 - LOW - HOLIDAY INNL" ||
+    selectedOption === "3502 - LOW - BEST WESTERN" ||
+    selectedOption === "3503 - LOW - SHERATON" ||
+    selectedOption === "3504 - LOW - HILTON" ||
+    selectedOption === "3508 - LOW - QUALITY INN" ||
+    selectedOption === "3509 - LOW - MARRIOTT" ||
+    selectedOption === "3512 - LOW - INTER-CONTINENTAL" ||
+    selectedOption === "3519 - LOW - PULLMAN INTERNATIONAL HOTELS" ||
+    selectedOption === "3528 - LOW - THUNDERBIRD/RED LION" ||
+    selectedOption === "3533 - LOW - HOTEL IBIS" ||
+    selectedOption === "3535 - LOW - HILTON INTERNATIONAL" ||
+    selectedOption === "3537 - LOW - ANA HOTELS" ||
+    selectedOption === "3512 - LOW - INTER-CONTINENTAL" ||
+    selectedOption === "3519 - LOW - PULLMAN INTERNATIONAL HOTELS" ||
+    selectedOption === "3528 - LOW - THUNDERBIRD/RED LION" ||
+    selectedOption === "3533 - LOW - HOTEL IBIS" ||
+    selectedOption === "3535 - LOW - HILTON INTERNATIONAL" ||
+    selectedOption === "3537 - LOW - ANA HOTELS" ||
+    selectedOption === "3542 - LOW - ROYAL HOTELS" ||
+    selectedOption === "3544 - LOW - CIGAHOTELS" ||
+    selectedOption === "3562 - LOW - COMFORT INN" ||
+    selectedOption === "3579 - LOW - HOTEL MERCURE" ||
+    selectedOption === "3595 - LOW - HOSPITALITY INNS" ||
+    selectedOption === "3612 - LOW - MOEVENPICK HOTELS" ||
+    selectedOption === "3615 - LOW - TRAVELODGE" ||
+    selectedOption === "3635 - LOW - RESO HOTELS" ||
+    selectedOption === "3640 - LOW - HYATT" ||
+    selectedOption === "3641 - LOW - SOFITEL HOTELS" ||
+    selectedOption === "3642 - LOW - NOVOTEL" ||
+    selectedOption === "3649 - LOW - RADISSON HOTEL" ||
+    selectedOption === "3652 - LOW - EMBASSY HOTELS" ||
+    selectedOption === "3661 - LOW - METROPOLE HOTELS" ||
+    selectedOption === "3690 - LOW - COURTYARD BY MARRIOTT" ||
+    selectedOption === "3692 - LOW - DOUBLETREE HOTEL" ||
+    selectedOption === "3695 - LOW - EMBASSY SUITES" ||
+    selectedOption === "3700 - LOW - MOTEL 6" ||
+    selectedOption === "3701 - LOW - LA MANSION DEL RIO" ||
+    selectedOption === "3704 - LOW - ROYCE HOTEL" ||
+    selectedOption === "3714 - LOW - FOUR SEASONS (AUSTRALIA)" ||
+    selectedOption === "3716 - LOW - CARLTON HOTELS" ||
+    selectedOption === "3717 - LOW - CITY LODGE HOTELS" ||
+    selectedOption === "3722 - LOW - WYNDHAM" ||
+    selectedOption === "3736 - LOW - COLORADO BELLE/EDGEWATER RESORT" ||
+    selectedOption === "3747 - LOW - CLUBCORP/CLUBRESORTS" ||
+    selectedOption === "3749 - LOW - THE BEVERLY HILLS HOTEL" ||
+    selectedOption === "3750 - LOW - CROWNE PLAZA HOTELS" ||
+    selectedOption === "3755 - LOW - THE HOMESTEAD" ||
+    selectedOption === "3763 - LOW - CHATEAU ELAN WINERY AND RESORT" ||
+    selectedOption === "3778 - LOW - FOUR POINTS HOTELS" ||
+    selectedOption === "3779 - LOW - W HOTELS" ||
+    selectedOption === "3785 - LOW - OUTRIGGER HOTELS AND RESORTS" ||
+    selectedOption === "3824 - LOW - ARIA" ||
+    selectedOption === "4214 - HIGH - MOVING AND STORAGE COMPANIES" ||
+    selectedOption === "4225 - HIGH - PUBLIC WAREHOUSING" ||
+    selectedOption === "4411 - HIGH - STEAMSHIP/CRUISE LINES" ||
+    selectedOption === "4457 - HIGH - BOAT RENTALS & LEASING" ||
+    selectedOption === "4722 - HIGH - TRAVEL AGENCIES AND TOUR OPERATORS" ||
+    selectedOption === "5094 - HIGH - PRECIOUS STONES/METALS/JEWELRY" ||
+    selectedOption === "5511 - HIGH - CAR & TRUCK DEALERS/NEW/USED-SALES/SERVICE/REPAIRS/PARTS AND LEASING" ||
+    selectedOption === "5521 - HIGH - CAR & TRUCK DEALERS/USED ONLY" ||
+    selectedOption === "5571 - HIGH - MOTORCYCLE DEALERS" ||
+    selectedOption === "5592 - LOW - MOTOR HOME DEALERS" ||
+    selectedOption === "5599 - LOW - MISCELLANEOUS AUTOMOTIVE/AIRCRAFT AND FARM EQUIPMENT DEALERS (NOT ELSEWHERE CLASSIFIED)" ||
+    selectedOption === "5712 - LOW - FURNITURE /HOME FURNISHINGS AND EQUIPMENT STORES (NOT APPLIANCES)" ||
+    selectedOption === "5713 - LOW - CARPET STORES" ||
+    selectedOption === "5713 - LOW - FLOOR COVERING STORES" ||
+    selectedOption === "5714 - LOW - DRAPERY & UPHOLSTERY STORES" ||
+    selectedOption === "5718 - LOW - FIREPLACES & ACCESSORIES" ||
+    selectedOption === "5719 - LOW - MISCELLANEOUS HOME FURNISHING SPECIALTY STORES" ||
+    selectedOption === "5944 - HIGH - JEWELRY STORES/WATCHES/CLOCKS AND SILVERWARE STORES" ||
+    selectedOption === "5992 - LOW - FLORISTS" ||
+    selectedOption === "6300 - LOW - INSURANCE SALES/UNDERWRITING AND PREMIUMS" ||
+    selectedOption === "6513 - LOW - REAL EST AGNTS & MGRS RENTALS" ||
+    selectedOption === "7011 - LOW - HOTELS/MOTELS/RESORTS/BED AND BREAKFASTS/CENTRAL RESERVATION SERVICES (NOT ELSEWHERE CLASSIFIED)" ||
+    selectedOption === "7032 - LOW - SPORTING/RECREATIONAL CAMPS" ||
+    selectedOption === "7033 - LOW - TRAILER PARKS/CAMPGROUNDS" ||
+    selectedOption === "7379 - HIGH - COMPUTER MAINTENANCE/REPAIR/SERVICE" ||
+    selectedOption === "7513 - LOW - TRUCK/UTILITY TRAILER RENTALS" ||
+    selectedOption === "7519 - LOW - MOTOR HOME RENTALS" ||
+    selectedOption === "7911 - LOW - DANCE HALLS/STUDIOS/SCHOOLS" ||
+    selectedOption === "7922 - HIGH - THEATRICAL PRODUCERS AND TICKET SALES (EVENTS)" ||
+    selectedOption === "7929 - HIGH - BANDS/ORCHESTRAS/ENTERTAINERS" ||
+    selectedOption === "7997 - LOW - MEMBERSHIP CLUBS (SPORTS/RECREATION/ATHLETIC), COUNTRY CLUBS AND PRIVATE GOLF COURSES" ||
+    selectedOption === "8211 - HIGH - ELEMENTARY/SECONDARY SCHOOLS" ||
+    selectedOption === "8220 - LOW - COLLEGES/UNIVERSITIES/JUNIOR COLLEGES/PROFESSIONAL SCHOOLS" ||
+    selectedOption === "8241 - HIGH - CORRESPONDENCE SCHOOLS" ||
+    selectedOption === "8244 - HIGH - BUSINESS/SECRETARIAL SCHOOL" ||
+    selectedOption === "8249 - HIGH - TRADE/VOCATIONAL SCHOOLS" ||
+    selectedOption === "8299 - HIGH - SCHOOLS AND EDUCATIONAL SERVICES (NOT CLASSIFIED ELSEWHERE)" ||
+    selectedOption === "8351 - LOW - CHILD CARE SERVICES") {
+    highRiskCommentsInput.value = "Merchants accepts payment at completion of work.";
+  } else {
+    highRiskCommentsInput.value = "";
+  }
+}
